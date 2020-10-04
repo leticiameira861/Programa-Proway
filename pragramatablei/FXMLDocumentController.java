827d8c48ca312484a049ae9ebfff7f5ae9152460
leticia <leticiameira861@gmail.com>
2020-10-04 18:08:47 -0300

    adicionado código do programa

---
+++ pragramatablei/FXMLDocumentController.java
@@ -0,0 +1,36 @@
+
+package pragramatablei;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.BorderPane;
+
+    
+  public abstract class FXMLDocumentController implements Initializable {
+  public class PleaseProvideControllerClassName {
+
+    @FXML
+    private BorderPane border;
+
+    @FXML
+    private TableColumn<?, ?> Column6MapKey;
+
+    @FXML
+    private TableColumn<?, ?> Column2MapKey;
+
+    @FXML
+    private TableColumn<?, ?> Column4MapKey;
+
+    @FXML
+    private TableColumn<?, ?> Column1MapKey;
+
+    @FXML
+    private TableColumn<?, ?> Column5MapKey;
+
+    @FXML
+    private TableColumn<?, ?> Column3MapKey;
+
+}}
+
